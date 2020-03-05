65e6585cfd5a29ff311e47b0a59ff1373fc2a4dc
nekmo <contacto@nekmo.com>
2020-03-05 18:34:04 +0100

    decorator and permission.

--- rest_framework_sudo/expiration.py
+++ rest_framework_sudo/expiration.py
@@ -1,8 +1,13 @@
 import datetime
+from functools import wraps
+
+from rest_framework.exceptions import PermissionDenied
 
 from rest_framework_sudo import settings
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
+
+from rest_framework_sudo.utils import get_request_argument
 
 
 def get_user_remaning_time(user: AbstractUser):
@@ -18,3 +23,15 @@ def get_user_remaning_time(user: AbstractUser):
 
 def get_expires_at(user: AbstractUser):
     return timezone.now() + get_user_remaning_time(user)
+
+
+def sudo_required(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        request = get_request_argument(args, kwargs)
+        if request is None:
+            raise AttributeError('request is not available')
+        if not get_user_remaning_time(request.user):
+            raise PermissionDenied('You need to authenticate again to perform this action.')
+        return fn(*args, **kwargs)
+    return wrapper
